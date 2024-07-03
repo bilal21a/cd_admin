556163bb76fbb6264c35bd96e1a7f7a707301ada
Bilal Mac <b.anwar21.a@gmail.com>
2024-07-03 21:31:15 +0500

    job get api

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/register', 'RegisterController@register')->name('register');
-Route::post('/common/jobs', 'CommonController@get_jobs');
+Route::get('/common/jobs', 'CommonController@get_jobs');
 
