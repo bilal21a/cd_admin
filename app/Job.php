db0770751338feeb2f5608b1751f6cbafa0dba69
Bilal Mac <b.anwar21.a@gmail.com>
2024-07-14 17:37:15 +0500

    w

--- app/Job.php
+++ app/Job.php
@@ -10,8 +10,8 @@ class Job extends Model
     {
         return $this->hasMany(Application::class);
     }
-    public function getJobImgAttribute()
+    public function getJobImgAttribute($value)
     {
-        return asset('storage/job_media/job_img/' . $this->attributes['job_img']);
+        return asset('storage/job_media/job_img/' . $value);
     }
 }
